f66b3d572d642fef79f969ac8de4dfd254d51c5d
jsmsue010 <jsmsue010@gmail.com>
2025-09-02 01:32:10 +0900

    연습문제 2/16

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,6 +4,6 @@ package org.example
 // 에디터 여백에 있는 <icon src="AllIcons.Actions.Execute"/> 아이콘을 클릭하세요.
 fun main() {
     //coreDrill()
-    winningScore();
+    winningScore()
     winningScore_answer()
 }
