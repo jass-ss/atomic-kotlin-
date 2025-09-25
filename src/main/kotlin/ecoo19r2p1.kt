77ebc81154b41bf0e42655de8f5602a4749b81dd
jsmsue010 <jsmsue010@gmail.com>
2025-09-26 01:51:20 +0900

    연습문제 12-1/16

---
+++ src/main/kotlin/ecoo19r2p1.kt
@@ -0,0 +1,51 @@
+package org.example
+
+//1. you can format a given address in several different ways.
+//  - The entire address is case-insensitive.
+//  - Dots (.) before the at-sign (@) sign are ignored.
+//  - A plus (+) followed by any string can be added before the at-sign (@).
+//    The plus and following string are ignored.
+// 2. determine the number of unique email addresses
+//  - The input will contain 10 datasets.
+//  - Each dataset begins with a line containing an integer N (1<=N<=100000), the number of email addresses
+//  - he next N lines each contain an email address S(1<=S<=30)
+//  - address will be formatted as a non-empty user part, a single at-sign followed by a non-empty domain part.
+//  - For the first 6 cases,N<=100.
+// ex :foo@bar.com and fO.o+baz123@bAR.com refer to the same email address.
+
+fun normalizeEmail(email:String):String{
+    val lowerEmail = email.lowercase()
+    val beforeAtSign = lowerEmail.split("@")[0]
+
+    //+인 경우 +부터 @전까지 무시
+    var i = 0
+    var noPlus = true
+    while(noPlus && i <= beforeAtSign.lastIndex ){
+       if (beforeAtSign[i] == '+'){
+           noPlus = false
+       }else{
+           i++
+       }
+    }
+    val removePlusStr = beforeAtSign.substring(0, i)
+    //println(removePlusStr)
+
+    var normalized: String = ""
+    //@ 앞에 있는 .들 모두 무시
+    for (t in removePlusStr){
+        if(t != '.') normalized += t
+    }
+    //println(normalized)
+    return normalized
+}
+
+fun ecoo19r2p1(){
+    val input = System.`in`.bufferedReader()
+    val output = System.out.bufferedWriter()
+
+    for(i in 1..4){
+        val str = input.readLine()
+        if(i != 1)normalizeEmail(str)
+    }
+
+}
